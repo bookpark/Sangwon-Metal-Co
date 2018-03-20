b76d923b73de5b8d8205aee2f46825b83e2f32b0
Booki Park <bkbkgg@gmail.com>
2018-03-21 01:16:10 +0900

    media url 추가

--- sangwon/config/urls.py
+++ sangwon/config/urls.py
@@ -13,12 +13,19 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 
-from products.views import product_list
+from products import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('products/', product_list, name='product-list'),
+    path('products/', views.product_list, name='product-list'),
 ]
+
+urlpatterns += static(
+    settings.MEDIA_URL,
+    document_root=settings.MEDIA_ROOT,
+)
