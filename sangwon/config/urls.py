d95fbcf369eae5129593a9321c6ebb300905f3ab
Booki Park <bkbkgg@gmail.com>
2018-03-21 00:58:42 +0900

    제품 리스트 View 작성

--- sangwon/config/urls.py
+++ sangwon/config/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from products.views import product_list
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('products/', product_list, name='product-list'),
 ]
