12bd3727a924a88a1098ecab6730d09a169e75bd
Booki Park <bkbkgg@gmail.com>
2018-05-23 17:52:28 +0900

    index view를 config로 이동

--- sangwon/products/views.py
+++ sangwon/products/views.py
@@ -3,10 +3,6 @@ from django.shortcuts import render
 from .models import Product
 
 
-def index(request):
-    return render(request, 'index.html')
-
-
 def product_list(request):
     products = Product.objects.all()
     context = {
